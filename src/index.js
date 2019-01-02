ec9d899a3d87ac01dcc5deba08d6dac511a6cacd
seankidwell <dev.seankidwell@gmail.com>
2019-01-02 13:22:53 -0500

    switched to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
 import Root from './screens/Root';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 // import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <Root />
-  </HashRouter>
+  </BrowserRouter>
   , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
